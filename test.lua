08f36cb11245935973b52da943dc41b9251b79ad
lordnikky <cuzimnikky@gmail.com>
2026-08-24 22:03:59 +0200

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,10 +1,1 @@
-local t = {}
-local old
-old = hookmetamethod(t, "__newindex", function(self, key, value)
-    print("newindex:", key, "=", value)
-    if key ~= "blocked" then
-        old(self, key, value)
-    end
-end)
-t.hello = "world"
-print(t.hello)   -- should print "world"
+printidentity()
