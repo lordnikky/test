0b875e756d0f30fbf5122f509b5b97788c205435
lordnikky <cuzimnikky@gmail.com>
2026-08-24 22:56:44 +0200

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,36 +1,23 @@
-local Params = {
- RepoURL = "https://raw.githubusercontent.com/luau/UniversalSynSaveInstance/main/",
- SSI = "saveinstance",
-}
-local synsaveinstance = loadstring(game:HttpGet(Params.RepoURL .. Params.SSI .. ".luau", true), Params.SSI)()
-local Options = {
-    Workspace = true,
-    Lighting = true,
-    ReplicatedFirst = true,
-    ReplicatedStorage = true,
-    StarterGui = true,
-    StarterPack = true,
-    StarterPlayer = true,
-    Teams = true,
-    SoundService = true,
-    Chat = true,
-    LocalizationService = true,
-    TestService = true,
-    Terrain = true,
-    Players = true,
-    Decompile = true,              -- MUST be true
-    DecompileTimeout = 10,
-    DecompileJobless = false,
-    SaveBytecode = false,
-    SaveNonCreatable = true,
-    SaveNotArchivable = true,
-    NilInstances = true,
-    IgnoreDefaultProperties = false,
-    SaveAssets = true,
-    SafeMode = true,
-    MaxThreads = 128,
-    ShowStatus = true,
-    Binary = true,
-    mode = "optimized",
-}
-synsaveinstance(Options)
+loadstring([[
+local cb = Instance.new("BindableFunction")
+local ans
+cb.OnInvoke = function(v)
+    ans = tostring(v)
+end
+
+local sg = game:GetService("StarterGui")
+sg:SetCore("SendNotification", {
+    Title = "UNC rconsole tests",
+    Text = "Some executors crash on rconsole. Test rconsole functions?",
+    Duration = 15,
+    Button1 = "Yes",
+    Button2 = "No",
+    Callback = cb,
+})
+
+local t0 = os.clock()
+while ans == nil and os.clock() - t0 < 15 do
+    task.wait(0.05)
+end
+print("RESULT:", ans or "no answer")
+]])()
