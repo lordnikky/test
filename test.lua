142123e63f03a2b9a8ee98696da0eefadd6870b6
lordnikky <cuzimnikky@gmail.com>
2026-08-24 20:54:35 +0200

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,1 +1,8 @@
-print("hello world")
+-- assuming `loadstring` refers to Fiu's loadstring
+loadstring([[
+local function original(x) return x + 1 end
+local function hook(x) return original(x) * 10 end
+local old = hookfunction(original, hook)
+print(original(5))  -- should print 60
+print(old(5))        -- should print 6
+]])()
