fcf01f4f342b0e9a5e10a5407fda300b471fcb39
lordnikky <cuzimnikky@gmail.com>
2026-08-26 21:21:27 +0200

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,12 +1,36 @@
 local Params = {
-    RepoURL = "https://raw.githubusercontent.com/luau/UniversalSynSaveInstance/main/",
-    SSI = "saveinstance",
+ RepoURL = "https://raw.githubusercontent.com/luau/UniversalSynSaveInstance/main/",
+ SSI = "saveinstance",
 }
 local synsaveinstance = loadstring(game:HttpGet(Params.RepoURL .. Params.SSI .. ".luau", true), Params.SSI)()
-local ok, err = xpcall(function()
-    synsaveinstance({})
-end, function(e)
-    return debug.traceback(e, 2)
-end)
-print("OK:", ok)
-print("ERR:", err)
+local Options = {
+    Workspace = true,
+    Lighting = true,
+    ReplicatedFirst = true,
+    ReplicatedStorage = true,
+    StarterGui = true,
+    StarterPack = true,
+    StarterPlayer = true,
+    Teams = true,
+    SoundService = true,
+    Chat = true,
+    LocalizationService = true,
+    TestService = true,
+    Terrain = true,
+    Players = true,
+    Decompile = true,              -- MUST be true
+    DecompileTimeout = 10,
+    DecompileJobless = false,
+    SaveBytecode = false,
+    SaveNonCreatable = true,
+    SaveNotArchivable = true,
+    NilInstances = true,
+    IgnoreDefaultProperties = false,
+    SaveAssets = true,
+    SafeMode = true,
+    MaxThreads = 128,
+    ShowStatus = true,
+    Binary = true,
+    mode = "optimized",
+}
+synsaveinstance(Options)
