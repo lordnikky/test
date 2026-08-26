2144a20da2b687f40353b6e0ee7e999aed2f7b15
lordnikky <cuzimnikky@gmail.com>
2026-08-26 20:39:25 +0200

    Update test2.lua

--- test2.lua
+++ test2.lua
@@ -4,9 +4,9 @@ local Params = {
 }
 local synsaveinstance = loadstring(game:HttpGet(Params.RepoURL .. Params.SSI .. ".luau", true), Params.SSI)()
 local ok, err = xpcall(function()
-    synsaveinstance({})  -- or your Options
+    synsaveinstance({})
 end, function(e)
     return debug.traceback(e, 2)
 end)
 print("OK:", ok)
-print(err)
+print("ERR:", err)
